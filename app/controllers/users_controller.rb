e821e8ec6a7bc033ff13db2f24c1a3a8d04b861f
itaruby_on_rails <itaruo93o@icloud.com>
2022-07-08 18:53:32 +0700

    revert Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,4 @@ def new
   @user = User.new
 end
 
-def create
-  @user = User.new(user_params)
-  if @user.save
-    flash[:notice] = "Welcome to the #{@user.username} Blog, you have successfully signup"
-    redirect_to articles_path
-  else
-    render 'new'
-  end
-end
-
-private
-def user_params
-  params.require(:user).permit(:username, :email, :password)
-end
-
 end
