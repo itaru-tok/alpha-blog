d629b21ae866011629ead302972502d00da567bc
itaruby_on_rails <itaruo93o@icloud.com>
2022-07-08 09:49:21 +0700

    Add bcrypt and has_secure_password to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_07_142947) do
+ActiveRecord::Schema.define(version: 2022_07_08_024028) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_07_07_142947) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
