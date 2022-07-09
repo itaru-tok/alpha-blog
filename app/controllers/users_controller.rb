8fa4bf23062ef0ef31ebfa90f9699f14df93c385
itaruby_on_rails <itaruo93o@icloud.com>
2022-07-09 08:02:57 +0700

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,29 @@
 class UsersController < ApplicationController
 
-def new
-  @user = User.new
-end
-
-def edit
-  @user = User.find(params[:id])
-end
-
-def update
-  @user = User.find(params[:id])
-  if @user.update(user_params)
-    flash[:notice] = "Your account information was successfully updated"
-    redirect_to articles_path
-  else
-    render 'edit'
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
   end
-end
 
-def create
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Itaru Blog #{@user.username}, you have successfully signed up"
